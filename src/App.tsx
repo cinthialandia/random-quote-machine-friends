4223639e5bd753a4359a9a3b9a6832f762312031
Cinthia <cinthia.j.23@gmail.com>
2020-05-28 20:55:29 +1000

    responsive design and colors ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
-import "./App.css";
+import "./App.scss";
+import { TwitterShareButton } from "react-share";
+
+const THEMES = ["red", "blue", "yellow"];
 
 function App() {
   const [quote, setQuote] = useState(
     "I grew up in a house with Monica, okay. If you didn't eat fast, you didn't eat."
   );
   const [character, setCharacter] = useState("Ross");
+  const [theme, setTheme] = useState("yellow");
 
   async function apiQuotes() {
     const url = "https://friends-quotes-api.herokuapp.com/quotes/random";
@@ -18,19 +22,30 @@ function App() {
     const result = await apiQuotes();
     setQuote(result.quote);
     setCharacter(result.character);
+    const nextThemeIndex = (THEMES.indexOf(theme) + 1) % THEMES.length;
+    setTheme(THEMES[nextThemeIndex]);
   }
 
   return (
-    <div className="App">
-      <div className="photoCharacter">
-        <img className="friends" src={`/img/${character}.png`} alt="monica" />
+    <div className={`App ${theme}`}>
+      <div className="container-logo">
+        <img
+          className="friends-logo"
+          src="/img/friends-logo.png"
+          alt="friends"
+        />
       </div>
-      <div className="container-bubble">
-        <div className="speech-bubble">
-          <div className="quote">{`“${quote}”`}</div>
-          <div className="character">{`-${character}`}</div>
-          <div className="buttons">
-            <button onClick={handleClick}>Random Quote</button>
+      <div className="container">
+        <div className="photoCharacter">
+          <img className="friends" src={`/img/${character}.png`} alt="monica" />
+        </div>
+        <div className="container-bubble">
+          <div className="speech-bubble">
+            <div className="quote">{`“${quote}”`}</div>
+            <div className="character">{`-${character}`}</div>
+            <div className="buttons">
+              <button onClick={handleClick}>Random Quote</button>
+            </div>
           </div>
         </div>
       </div>
