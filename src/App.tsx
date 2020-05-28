b1b009ff7179c47ff0d687910ea24c8f2399cf22
Cinthia <cinthia.j.23@gmail.com>
2020-05-28 15:25:37 +1000

    the quote random works!

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [quote, setQuote] = useState(
+    "I grew up in a house with Monica, okay. If you didn't eat fast, you didn't eat."
+  );
+  const [character, setCharacter] = useState("Ross");
+
+  async function apiQuotes() {
+    const url = "https://friends-quotes-api.herokuapp.com/quotes/random";
+    let response = await fetch(url);
+    let data = await response.json();
+    return data;
+  }
+
+  async function handleClick() {
+    const result = await apiQuotes();
+    setQuote(result.quote);
+    setCharacter(result.character);
+    console.log(result);
+  }
+
+  //const friends = [];
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <img
+            className="friends-logo"
+            src="/img/friends-logo.png"
+            alt="friends-logo"
+          />
+        </div>
+
+        <div>Aqui va: {quote}</div>
+        <div>by: {character}</div>
+        <button onClick={handleClick}>Random Quote</button>
       </header>
     </div>
   );
